cc48239fc96def593d15f17d9aaf1200b42cb334
joe <>
2018-08-26 11:15:10 +0800

    Joy Check

---
+++ Assets/Joe_script/CheckJoy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckJoy : MonoBehaviour {
+    public List<PlayerMove> players;
+    public int p = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0+p; i < 6; i++ ){
+            if (Input.GetButtonDown("joystick" + i + "Fire"))
+            {
+                players[0].num = i;
+                players[0].gameObject.SetActive(true);
+                p++;
+                break;
+            }
+
+        }
+	}
+}
